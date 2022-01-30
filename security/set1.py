3c7db5303c337830efad8ce5ef51b704cc1ba01e
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2022-01-30 14:15:45 -0900

    finally finished set 1

--- security/set1.py
+++ security/set1.py
@@ -63,7 +63,7 @@ def base64_2hex(str):
         if ord(c) == ord("+"):
             b.append(62)
             continue
-        b.append(ord("/"))
+        b.append(63)
 
     if not ((len(b) % 2) == 0):
         b.insert(0,0)
@@ -370,3 +370,22 @@ for key_size in possible_keysizes:
 
     print("Key = " + key)
     print("Msg = " + msg)
+
+print("\nChallange 8)")
+lines_ = open("data_c8.txt", "r").read().splitlines()
+line_no = 0
+for line in lines_:
+    i = 0
+    bytes = []
+    while i < len(line):
+        bytes.append(ord(line[i])*16 + ord(line[i+1]))
+        i += 2
+
+    # now just do a O(n^2) pairwaise check of all 16 byte blocks
+    n_blocks = len(bytes) / 16
+    for i in range(0, n_blocks):
+        for j in range(i+1, n_blocks):
+            if bytes[i*16 : (i+1)*16] == bytes[j*16 : (j+1)*16]:
+                print("Found AES = line " + str(line_no))
+
+    line_no += 1
