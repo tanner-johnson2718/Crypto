6bd20837613f9acbbba1601c5c5d25b98276bbc4
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2022-02-10 12:12:53 -0900

    bruh gcd

--- set3/euclid.py
+++ set3/euclid.py
@@ -1,38 +1,72 @@
+###############################################################################
 # Euclidean Algorithm
+###############################################################################
+import matplotlib.pyplot as plt
+import numpy as np
+
 def gcd(a,b):
     if b == 0:
         return a
     return gcd(b, a%b)
 
-# Solve a = 1 mod m  <==>  n_1*a + n_2*m = 1
-def modInverse(a, b):
-    y_ = 0
-    n1 = 1
- 
-    if (b == 1):
-        return 0
-    while (a > 1):
- 
-        # q is quotient
-        q = a // b
- 
-        t_ = b
- 
-        # m is remainder now, process
-        # same as Euclid's algo
+def gcd_iter_tracking(a,b):
+    n = 0
+    while b > 0:
+        n += 1
+        t = b
         b = a % b
-        a = t_
-        t_ = y_
- 
-        # Update x and y
-        y_ = n1 - q * y_
-        n1 = t_
+        a = t
+    return a, n
 
-        print("a="+str(a)+" b="+str(b)+" n1="+str(n1))
+a_list = range(2,1000)
+b_list = range(2,1000)
+nmap = np.zeros((1000,1000))
 
-    return n1
+for a_in in a_list:
+    for b_in in b_list:
+        g,n = gcd_iter_tracking(a_in, b_in)
+        nmap[a_in][b_in] = n
+
+fig, ax = plt.subplots()
+pcm = ax.pcolormesh(nmap)
+fig.colorbar(pcm)
+plt.show()
+###############################################################################
+# Extended Euclidean Algorithm
+###############################################################################
+# Solve a = 1 mod b and b =1 mod a <==>  n_a*a + n_b*b = 1
+# return n_a, n_b or inv(a), inv(b) in mod the other variable
+def modInverse(a, b):
+
+    if b > a:
+        n_b, n_a = modInverse(b, a)
+        return n_a, n_b
+    
+    # Compute all the quotients using the same update rule as gcd: a becomes b
+    # and b becomes a%b. 
+    q = []
+    while b > 1:
+        q.append(a // b)
+        a_old  = a
+        b_old = b
+        a = b_old
+        b = a_old % b_old
+
+    # See write up for details on the update step.
+    n_a = 1
+    n_b = -q.pop()
+    while len(q) > 0:
+        n_a_old = n_a
+        n_b_old = n_b
+
+        n_a = n_b_old
+        n_b = n_a_old - (q.pop() * n_b_old)
+
+    return n_a, n_b
+    
+    
 
 # Mod inverse testing
-a = 17
-b = 29
+a = 35
+b = 97
 print("a = " + str(a) + " b = " + str(b) + " | inv = " + str(modInverse(a,b)))
